b4742b79cbb2064c21067f0383610fb75dc78402
jojhan08 <jojhan044@gmail.com>
2022-06-29 14:40:19 -0500

    backfront

--- my-store2.0/routes/index.js
+++ my-store2.0/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const productsRouter = require('./productsRouter')
-const usersRouter = require('./usersRouter')
+// const usersRouter = require('./usersRouter')
 const categoriesRouter = require('./categoriesRouter')
 
 
@@ -8,7 +8,7 @@ function routerApi(app){
     const router = express.Router();
     app.use('/api/v1/', router)
     router.use('/products', productsRouter)
-    router.use('/users', usersRouter)
+    // router.use('/users', usersRouter)
     router.use('/categories', categoriesRouter)
 }
 
